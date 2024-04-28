5f00ce2da26ee61ea0457e40f9995dec1f9071cb
Edizon <edizon1987@gmail.com>
2024-04-27 18:01:56 -0600

    166.- Creando el formulario principal

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+import Formulario from './components/Formulario.vue';
 </script>
 
-<template>
 
-</template>
 
 <style scoped>
 
 </style>
+
+<template>
+  <h1 class="titulo">Buscador de clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario 
+    />
+  </div>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {} from '../../public/style.css';
+const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+
+<template>
+    <form
+    class="formulario"
+    >
+    <div class="campo">
+        <label for="ciudad">Ciudad</label>
+        <input 
+        type="text" 
+        name="ciudad" 
+        placeholder="ciudad"
+        />
+    </div>
+
+    <div class="campo">
+        <label for="pais">País</label>
+        <select 
+        id="pais"
+        >
+            <option value="">--Seleccione--</option>
+            <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+        </select>
+    </div>
+
+    <input 
+    type="submit" 
+    value="Consultar clima"
+    />
+
+    </form>
+</template>

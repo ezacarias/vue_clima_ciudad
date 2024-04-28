43d875ca044a14e831d62d552ee4b04c2e4db795
Edizon <edizon1987@gmail.com>
2024-04-27 18:35:21 -0600

    168.- Validando el formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {} from '../../public/style.css';
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import Alerta from './Alerta.vue'
 
 const busqueda = reactive({
     ciudad:'',
@@ -15,12 +16,27 @@ const paises = [
     { codigo: 'ES', nombre: 'España' },
     { codigo: 'PE', nombre: 'Perú' }
 ]
+
+const consultarClima = ()=>{
+    if(Object.values(busqueda).includes('')){
+        error.value = 'Todos los campos son obligatorios'
+        return
+    }
+    error.value = ''
+}
+
+const error = ref('')
 </script>
 
 <template>
     <form
     class="formulario"
+    @submit.prevent="consultarClima"
     >
+    <Alerta v-if="error">
+    {{ error }}
+    </Alerta>
+
     <div class="campo">
         <label for="ciudad">Ciudad</label>
         <input 

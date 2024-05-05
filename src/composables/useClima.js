1b0b489d4c4aab6fc143b018d1490064842f00ee
Edizon <edizon1987@gmail.com>
2024-05-05 06:40:32 -0600

    172.- Primer llamado hacia el API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,9 +1,17 @@
 export default function useClima(){
-   
-   const obtenerClima = ({ciudad, pais})=>{
-     const key = import.meta.key
+    
+   const obtenerClima=({ ciudad, pais})=>{
+       //importar apikey
+       const key =import.meta.env.VITE_API_KEY;
+       //obtener la lat, lngt
+       const ur =`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}
+                   &limit=1&appid=${key}`;
+       
+       //obtener el clima
    }
-    return {
-       obtenerClima
-    }
+
+   return{
+       obtenerClima,
+   };
+
 }

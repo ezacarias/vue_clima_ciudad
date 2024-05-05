fc9747f429cd259ceceda6eb1e94041342219738
Edizon <edizon1987@gmail.com>
2024-05-05 06:44:03 -0600

    174.- Consultando el clima

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -10,8 +10,14 @@ export default function useClima(){
             &limit=1&appid=${key}`;
             const { data} = await axios (url);
             const {lat,lon}=data[0];
+            const urlClima=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
             
-            
+            const dataClima= await axios(urlClima);
+            const { data: resultado } = dataClima;
+            console.log(resultado);
+
+
+
 
         } catch (error) {
             console.log(error);
